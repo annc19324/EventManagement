218dd710fbff0e86b882d94d1ade29f7067cf25a
annc19324 <annc19324@gmail.com>
2024-11-01 13:30:42 +0700

    Update Connect.java

--- src/controller/Connect.java
+++ src/controller/Connect.java
@@ -4,10 +4,32 @@
  */
 package controller;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author annc1
  */
 public class Connect {
-    
+    public Connection conn = null;
+    public Connection connectSQL() throws SQLException {
+        try {
+            String userName = "av";
+            String password = "123";  
+            String url = "jdbc:sqlserver://localhost:1433; database=EventManagement; encrypt = true; TrustServerCertificate = true";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            conn = DriverManager.getConnection(url, userName, password);
+            System.out.println("Kết nối thành công tới CSDL 'EventManagement'");
+            
+            return conn;
+        } catch (ClassNotFoundException e) {
+            JOptionPane.showMessageDialog(null, "Kết nối CSDL thất bại: " + e.getMessage(), "Thông báo", 1);
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Không kết nối được CSDL: " + ex.getMessage(), "Thông báo", 1);
+        }
+        return null;
+    }
 }
