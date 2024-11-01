211703885efd0284c1d94a3d55e95a51ade3021d
annc19324 <annc19324@gmail.com>
2024-11-01 13:33:54 +0700

    Create Connect.java

---
+++ src/controller/Connect.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author annc1
+ */
+public class Connect {
+        public Connection conn = null;
+    public Connection connectSQL() throws SQLException {
+        try {
+            String userName = "sa";
+            String password = "12345";  
+            String url = "jdbc:sqlserver://localhost:1433; database=EventManagement; encrypt = true; TrustServerCertificate = true";
+
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            
+            conn = DriverManager.getConnection(url, userName, password);
+            System.out.println("Kết nối thành công tới CSDL 'EventManagement'");
+            return conn;
+        } catch (ClassNotFoundException e) {
+            JOptionPane.showMessageDialog(null, "Kết nối CSDL thất bại: " + e.getMessage(), "Thông báo", 1);
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Không kết nối được CSDL: " + ex.getMessage(), "Thông báo", 1);
+        }
+        return null;
+    }
+}
